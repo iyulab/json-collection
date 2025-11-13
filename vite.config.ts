81818b23df033dec8935722edbe239eb9134159f
iujunhyung <junehyung@iyulab.com>
2025-11-13 19:30:02 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest/config" />
-import { defineConfig, PluginOption } from 'vite';
+import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
 
@@ -10,6 +10,10 @@ export default defineConfig({
     include: ['tests/**/*.{test,spec}.{js,ts}'],
   },
   build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    minify: true,
+    sourcemap: true,
     lib: {
       name: 'JsonCollection',
       entry: resolve(__dirname, 'src/index.ts'),
@@ -17,24 +21,20 @@ export default defineConfig({
       fileName: (format) => format === 'es' ? 'index.js' : `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['mathjs'],
+      external: [
+        'mathjs'
+      ],
       output: {
         globals: {
           mathjs: 'mathjs',
         },
       }
     },
-    outDir: 'dist',
-    emptyOutDir: true,
-    minify: true,
-    sourcemap: true,
   },
   plugins: [
     dts({
-      outDir: 'dist',
-      tsconfigPath: resolve(__dirname, 'tsconfig.json'),
+      include: ['src/**/*'],
       rollupTypes: true,
-      insertTypesEntry: true,
-    }) as PluginOption,
+    }),
   ],
 });
